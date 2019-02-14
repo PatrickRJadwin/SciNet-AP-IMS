3cf74dc4f07abc9df74f2ac83d88e778898dbd89
Cory Merrick <cmerrick02@gmail.com>
2019-02-14 17:34:02 -0500

    Added functions for opening dialog
    functions for opening dialog, and imported matdialog and additemcomponent

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
+import { MatSort, MatTableDataSource, MatDialog, MatDialogRef } from '@angular/material';
+import { AdditemComponent } from '../additem/additem.component';
 
 export interface PeriodicElement {
   seqNo: number;
@@ -22,13 +23,13 @@ const date = new Date(dateString);
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {seqNo: 1, name: 'AP-BDE4', location: 'C-1', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
-  {seqNo: 2, name: 'AP-BDX9', location: 'C-3', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
+  {seqNo: 2, name: 'AP-BDX9', location: 'C-3', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: false, edit: 'edit', trash: 'trash'},
   {seqNo: 3, name: 'AP-BDS7', location: 'C-3', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
   {seqNo: 4, name: 'AP-WB5', location: 'C-2', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
   {seqNo: 5, name: 'AP-BSB1', location: 'C-1', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
-  {seqNo: 6, name: 'AP-ZDB3', location: 'C-2', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
-  {seqNo: 7, name: 'AP-BDC1', location: 'C-1', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
-  {seqNo: 8, name: 'AP-BDB2', location: 'C-5', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: true, edit: 'edit', trash: 'trash'},
+  {seqNo: 6, name: 'AP-ZDB3', location: 'C-2', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: false, complete: true, edit: 'edit', trash: 'trash'},
+  {seqNo: 7, name: 'AP-BDC1', location: 'C-1', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: false, complete: true, edit: 'edit', trash: 'trash'},
+  {seqNo: 8, name: 'AP-BDB2', location: 'C-5', port: 'Desktop Switch', created_at: date, created_by: 'undefined', joined: true, complete: false, edit: 'edit', trash: 'trash'},
 
 ];
 // **Sample data**
@@ -48,6 +49,19 @@ export class InventoryComponent implements OnInit {
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AdditemComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
 
   ngOnInit() {
     this.dataSource.sort = this.sort;
