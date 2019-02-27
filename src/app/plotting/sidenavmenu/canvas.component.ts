b9b26c7c4e9507b2d72cbfae48d5e4c7eee63996
Josh McMahan <33476442+JGMacs@users.noreply.github.com>
2019-02-27 12:30:03 -0500

    Tentative canvas
    At this point, I'm thinking of replacing canvas with a div system similar to my project in Internet Software Dev.
    
    Talk more about this later.

---
+++ src/app/plotting/sidenavmenu/canvas.component.ts
@@ -0,0 +1,77 @@
+
+import {
+  Component, Input, ElementRef, AfterViewInit, ViewChild
+} from '@angular/core';
+import {fromEvent} from 'rxjs';
+import {pairwise, switchMap, takeUntil} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-canvas',
+  template: '<canvas #canvas></canvas>',
+  styles: ['canvas { border: 3px solid #000; background-color: white; border-color: dimgrey; border-style: groove; margin: 10px;}']
+})
+export class CanvasComponent implements AfterViewInit {
+  //  a reference to the canvas element from our template
+  @ViewChild('canvas') public canvas: ElementRef;
+
+  // setting a width and height for the canvas
+  @Input() public width = 1515;
+  @Input() public height = 700;
+
+  private cx: CanvasRenderingContext2D;
+
+  public ngAfterViewInit() {
+    // get the context
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+
+    // set the width and height
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+
+    // we'll implement this method to start capturing mouse events
+    this.captureEvents(canvasEl);
+  }
+
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    fromEvent(canvasEl, 'mousedown').pipe(
+      switchMap((e) => {
+        // after a mouse down, we'll record all mouse moves
+        return fromEvent(canvasEl, 'mousemove')
+          .pipe(
+            // we'll stop (and unsubscribe) once the user releases the mouse
+            // this will trigger a 'mouseup' event
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            // pairwise lets us get the previous value to draw a line from
+            // the previous point to the current point
+            pairwise()
+          );
+      })
+    )
+      .subscribe((res: [MouseEvent, MouseEvent]) => {
+        const rect = canvasEl.getBoundingClientRect();
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].clientX - rect.left,
+          y: res[0].clientY - rect.top
+        };
+
+        const currentPos = {
+          x: res[1].clientX - rect.left,
+          y: res[1].clientY - rect.top
+        };
+
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(currentPos);
+      });
+  }
+
+  private drawOnCanvas(currentPos: { x: number, y: number }) {
+    const deviceIcon = new Image();
+    deviceIcon.src = 'https://i.imgur.com/Q5skAxA.png';
+    this.cx.drawImage(deviceIcon, currentPos.x, currentPos.y, 100, 100);
+  }
+}
