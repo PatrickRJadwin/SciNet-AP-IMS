3418cddb8d85261899cfb5c3e9244e8b3bd14adb
Josh McMahan <33476442+JGMacs@users.noreply.github.com>
2019-02-27 12:27:43 -0500

    Create sidenavmenu.component.ts

---
+++ src/app/plotting/sidenavmenu/sidenavmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenavmenu.component.html',
+  styleUrls: ['./sidenavmenu.component.scss']
+})
+export class SidenavmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
