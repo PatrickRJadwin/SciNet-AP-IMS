9222e7e03db3b0ac79aaa43a35db475ccad7f7b1
Cory Merrick <cmerrick02@gmail.com>
2019-02-28 12:08:47 -0500

    Create inventory.service.ts

---
+++ src/app/inventory/inventory.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { Item } from './inventory.model';
+import { FirebaseApp} from 'angularfire2' ;
+import 'firebase/storage';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+  item: Item;
+  itemList: AngularFireList<any[]>;
+  constructor(private db: AngularFireDatabase, private firebaseApp: FirebaseApp) {
+
+    this.itemList = db.list('items');
+
+  }
+
+  getAllItems() {
+    return this.itemList;
+  }
+}
