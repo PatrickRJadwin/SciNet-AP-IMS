5578f93d571ff9db81c24ee4ff2cadf465cdb784
Cory Merrick <cmerrick02@gmail.com>
2019-03-01 18:45:16 -0500

    Added functions for filters and checkbox toggles

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import { MatSort, MatTableDataSource, MatDialog, MatDialogRef } from '@angular/material';
+import { MatSort, MatTableDataSource, MatDialog, MatDialogRef, MatPaginator } from '@angular/material';
 import { AdditemComponent } from '../additem/additem.component';
 import { InventoryService } from './inventory.service';
 import { Item } from './inventory.model';
@@ -24,10 +24,13 @@ export class InventoryComponent implements OnInit {
   dataSource = new MatTableDataSource();
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+
 
   constructor(public dialog: MatDialog, private inventoryService: InventoryService) {
     inventoryService.getAllItems();
@@ -67,14 +70,15 @@ export class InventoryComponent implements OnInit {
       });
       this.dataSource = new MatTableDataSource(this.itemList);
       this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
     });
   }
+
 
   //Delete/Edit functions
   onDelete(key: string) {
     this.inventoryService.deletebyKey(key);
   }
-
   
   selectItem(key: string, modal: string) {
     this.selectedItem = this.itemList.filter(x => x.$key === key)[0];
@@ -86,6 +90,45 @@ export class InventoryComponent implements OnInit {
     console.log(this.edit);
   }
 
+  selectJoinedToggle(key: string) {
+    this.selectedItem = this.itemList.filter(x => x.$key === key)[0];
+    if (this.selectedItem.joined === true) {
+      this.edit.joined = false;
+      let editedItem = new Item(this.selectedItem.mac, this.selectedItem.location, this.selectedItem.port, this.selectedItem.created_at,
+        this.selectedItem.created_by, this.edit.joined, this.selectedItem.complete, this.selectedItem.checkedIn);
+      editedItem.lastUpdate = new Date().toString();
+  
+      this.inventoryService.editItem(this.selectedItem.$key, editedItem);
+    }
+    else {
+      this.edit.joined = true;
+      let editedItem = new Item(this.selectedItem.mac, this.selectedItem.location, this.selectedItem.port, this.selectedItem.created_at,
+        this.selectedItem.created_by, this.edit.joined, this.selectedItem.complete, this.selectedItem.checkedIn);
+      editedItem.lastUpdate = new Date().toString();
+  
+      this.inventoryService.editItem(this.selectedItem.$key, editedItem);
+    }
+  }
+
+  selectCompleteToggle(key: string) {
+    this.selectedItem = this.itemList.filter(x => x.$key === key)[0];
+    if (this.selectedItem.complete === true) {
+      this.edit.complete = false;
+      let editedItem = new Item(this.selectedItem.mac, this.selectedItem.location, this.selectedItem.port, this.selectedItem.created_at,
+        this.selectedItem.created_by, this.selectedItem.joined, this.edit.complete, this.selectedItem.checkedIn);
+      editedItem.lastUpdate = new Date().toString();
+  
+      this.inventoryService.editItem(this.selectedItem.$key, editedItem);
+    }
+    else {
+      this.edit.joined = true;
+      let editedItem = new Item(this.selectedItem.mac, this.selectedItem.location, this.selectedItem.port, this.selectedItem.created_at,
+        this.selectedItem.created_by, this.selectedItem.joined, true, this.selectedItem.checkedIn);
+      editedItem.lastUpdate = new Date().toString();
+  
+      this.inventoryService.editItem(this.selectedItem.$key, editedItem);
+    }
+  }
 
   onSave() {
     let editedItem = new Item(this.edit.mac, this.edit.location, this.edit.port, this.selectedItem.created_at,
@@ -95,4 +138,69 @@ export class InventoryComponent implements OnInit {
     this.inventoryService.editItem(this.selectedItem.$key, editedItem);
   }
 
+
+    //Query functions for mat-select
+  getJoined() {
+    let data = this.inventoryService.getJoined();
+    data.snapshotChanges().subscribe(item => {
+        this.itemList = [];
+  
+      item.forEach(element => {
+          let json = element.payload.toJSON();
+          json["$key"] = element.key;
+          this.itemList.push(json as Item);
+        });
+      this.dataSource = new MatTableDataSource(this.itemList);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      });
+    }
+  
+  getComplete() {
+    let data = this.inventoryService.getCompleted();
+    data.snapshotChanges().subscribe(item => {
+        this.itemList = [];
+  
+      item.forEach(element => {
+          let json = element.payload.toJSON();
+          json["$key"] = element.key;
+          this.itemList.push(json as Item);
+        });
+      this.dataSource = new MatTableDataSource(this.itemList);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      });
+    }
+
+  getNotJoined() {
+    let data = this.inventoryService.getNotJoined();
+    data.snapshotChanges().subscribe(item => {
+          this.itemList = [];
+    
+      item.forEach(element => {
+            let json = element.payload.toJSON();
+            json["$key"] = element.key;
+            this.itemList.push(json as Item);
+          });
+        this.dataSource = new MatTableDataSource(this.itemList);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        });
+    }
+  
+  getJoinedNotCompleted() {
+    let data = this.inventoryService.getJoinedNotCompleted();
+    data.snapshotChanges().subscribe(item => {
+          this.itemList = [];
+    
+      item.forEach(element => {
+            let json = element.payload.toJSON();
+            json["$key"] = element.key;
+            this.itemList.push(json as Item);
+          });
+      this.dataSource = new MatTableDataSource(this.itemList);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      });
+  }
 }
