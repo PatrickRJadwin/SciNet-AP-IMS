7af958a65750f05f11e82726d68920c0bad61506
Cory Merrick <cmerrick02@gmail.com>
2019-03-05 10:12:05 -0500

    additem function
    need to get User's name for created_by field

--- src/app/additem/additem.component.ts
+++ src/app/additem/additem.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Injectable, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
+import { InventoryService } from '../inventory/inventory.service';
+import { Item } from '../inventory/inventory.model';
 
 @Component({
   selector: 'app-additem',
@@ -8,12 +10,25 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
 })
 export class AdditemComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<AdditemComponent>) { }
+  item = new Item("","","","","user",true,true,true);
+  add = new Item("","","","","user",true,true,true);
+
+  constructor(public dialogRef: MatDialogRef<AdditemComponent>, public inventoryService: InventoryService) { }
 
   public closeMe() {
     this.dialogRef.close();
   }
   ngOnInit() {
+  }
+
+  //Need to add logged in user's name for created_by field
+  addItem() {
+    this.item.mac = this.add.mac;
+    this.item.location = this.add.location;
+    this.item.port = this.add.port;
+    this.item.created_at = new Date().toString();
+    this.inventoryService.addItem(this.item);
+    this.dialogRef.close();
   }
 
 }
