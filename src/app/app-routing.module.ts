2c142a145c4c7a75b854bb1e58a8efaba7a942cb
Cory Merrick <cmerrick02@gmail.com>
2019-03-25 20:35:14 -0400

    Add authguard, canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { ReportsComponent } from './reports/reports.component';
 import { UsersComponent } from './users/users.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'inventory', component: InventoryComponent},
-  { path: 'plot', component: PlottingComponent},
-  { path: 'reports', component: ReportsComponent},
-  { path: 'users', component: UsersComponent},
+  { path: 'inventory', component: InventoryComponent, canActivate: [AuthGuard]},
+  { path: 'plot', component: PlottingComponent, canActivate: [AuthGuard]},
+  { path: 'reports', component: ReportsComponent, canActivate: [AuthGuard]},
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignUpComponent}
 ];
