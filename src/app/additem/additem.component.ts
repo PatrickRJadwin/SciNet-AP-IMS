ddc1e249bc5a2f4befd858537e568968a6a0ef23
Cory Merrick <cmerrick02@gmail.com>
2019-03-25 20:44:12 -0400

    added created_by user displayname

--- src/app/additem/additem.component.ts
+++ src/app/additem/additem.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Injectable, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material';
 import { InventoryService } from '../inventory/inventory.service';
 import { Item } from '../inventory/inventory.model';
+import { SnackbarService } from '../shared/snackbar.service';
+import { AuthenticationService } from '../shared/services/authentication.service';
 
 @Component({
   selector: 'app-additem',
@@ -13,7 +15,10 @@ export class AdditemComponent implements OnInit {
   item = new Item("","","","","user",true,true,true);
   add = new Item("","","","","user",true,true,true);
 
-  constructor(public dialogRef: MatDialogRef<AdditemComponent>, public inventoryService: InventoryService) { }
+  constructor(public dialogRef: MatDialogRef<AdditemComponent>,
+              public inventoryService: InventoryService,
+              public snack: SnackbarService,
+              public auth: AuthenticationService) { }
 
   public closeMe() {
     this.dialogRef.close();
@@ -23,12 +28,20 @@ export class AdditemComponent implements OnInit {
 
   //Need to add logged in user's name for created_by field
   addItem() {
+    if(this.auth.canDelete() === false) {
+      this.snack.openSnackBar('You do not have permissions for this', 2000);
+      this.dialogRef.close();
+    }
+    else {
+    const user = this.auth.getUser();
+    this.item.created_by = user.displayName;
     this.item.mac = this.add.mac;
     this.item.location = this.add.location;
     this.item.port = this.add.port;
     this.item.created_at = new Date().toString();
     this.inventoryService.addItem(this.item);
     this.dialogRef.close();
+    }
   }
 
 }
