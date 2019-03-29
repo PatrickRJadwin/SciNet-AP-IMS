65fa458add9dc46c5fa8a8b08c9a04172d2ba92d
Patrick Jadwin <prjadwin@pstcc.edu>
2019-03-28 21:01:10 -0400

    update

--- src/assets/CONFIGmain.ts
+++
@@ -1,9 +0,0 @@
-export default
-{
-    "apiKey": "AIzaSyD1DWBebZOAGk5VhC2fkaapjQpUrluctIM",
-    "authDomain": "scinet-ap-ims.firebaseapp.com",
-    "databaseURL": "https://scinet-ap-ims.firebaseio.com",
-    "projectId": "scinet-ap-ims",
-    "storageBucket": "scinet-ap-ims.appspot.com",
-    "messagingSenderId": "642944138783"
-}
