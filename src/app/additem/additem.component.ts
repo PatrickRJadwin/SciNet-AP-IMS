a3dea11e95796a9ff9bf0d62f26d257906292aea
Cory Merrick <cmerrick02@gmail.com>
2019-03-28 20:46:44 -0400

    changed if statement, calling function directly

--- src/app/additem/additem.component.ts
+++ src/app/additem/additem.component.ts
@@ -4,6 +4,8 @@ import { InventoryService } from '../inventory/inventory.service';
 import { Item } from '../inventory/inventory.model';
 import { SnackbarService } from '../shared/snackbar.service';
 import { AuthenticationService } from '../shared/services/authentication.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-additem',
@@ -15,10 +17,14 @@ export class AdditemComponent implements OnInit {
   item = new Item("","","","","user",true,true,true);
   add = new Item("","","","","user",true,true,true);
 
+  public tf: boolean;
+
   constructor(public dialogRef: MatDialogRef<AdditemComponent>,
               public inventoryService: InventoryService,
               public snack: SnackbarService,
-              public auth: AuthenticationService) { }
+              public auth: AuthenticationService,
+              private afAuth: AngularFireAuth,
+              private db: AngularFireDatabase) { }
 
   public closeMe() {
     this.dialogRef.close();
@@ -28,11 +34,16 @@ export class AdditemComponent implements OnInit {
 
   //Need to add logged in user's name for created_by field
   addItem() {
-    if(this.auth.isUser() == false) {
+    this.db.database.ref('/users/').child(this.afAuth.auth.currentUser.uid).child('role')
+    .child('user').once('value').then(snapshot => {
+      this.tf = snapshot.val();
+      console.log(this.tf);
+    })
+
+    if(this.tf === false) {
       this.snack.openSnackBar('You do not have permissions for this', 2000);
-      this.dialogRef.close();
     }
-    else if(this.auth.isUser() == true) {
+    else if(this.tf === true) {
     const user = this.auth.getUser();
     this.item.created_by = user.displayName;
     this.item.mac = this.add.mac;
@@ -41,7 +52,7 @@ export class AdditemComponent implements OnInit {
     this.item.created_at = new Date().toString();
     this.inventoryService.addItem(this.item);
     this.dialogRef.close();
-    }
+    };
   }
 
 }
