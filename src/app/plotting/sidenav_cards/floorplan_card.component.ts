6824660b0a2cdb1382d50c93389b2fb643b5bc35
Cory Merrick <cmerrick02@gmail.com>
2019-04-08 22:53:44 -0400

    populate sidenav

--- src/app/plotting/sidenav_cards/floorplan_card.component.ts
+++ src/app/plotting/sidenav_cards/floorplan_card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SidenavmenuComponent} from '../sidenavmenu/sidenavmenu.component';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+import { ImageModel } from 'src/app/admin/image.model';
+import { ImageService } from 'src/app/shared/services/image.service';
 
 @Component({
   selector: 'app-floorplancard',
@@ -8,9 +11,23 @@ import {SidenavmenuComponent} from '../sidenavmenu/sidenavmenu.component';
 })
 export class FloorplancardComponent implements OnInit {
 
-  constructor() { }
+  itemList: ImageModel[];
+
+  constructor(private db: AngularFireDatabase,
+              private image: ImageService) { }
 
   ngOnInit() {
+    let data = this.image.getLinks();
+    data.snapshotChanges().subscribe(item => {
+      this.itemList = [];
+
+      item.forEach(element => {
+        let json = element.payload.toJSON();
+        json["$key"] = element.key;
+        this.itemList.push(json as ImageModel);
+      });
+    });
+    
   }
 
   floorplanClicked(el) {
