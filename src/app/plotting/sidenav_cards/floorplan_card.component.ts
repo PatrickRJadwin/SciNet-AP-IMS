fcc53299b602fda09a34de547bccf63096a66238
Cory Merrick <cmerrick02@gmail.com>
2019-04-23 08:31:38 -0400

    added deletion of json data

--- src/app/plotting/sidenav_cards/floorplan_card.component.ts
+++ src/app/plotting/sidenav_cards/floorplan_card.component.ts
@@ -5,6 +5,7 @@ import { ImageModel } from 'src/app/shared/image.model';
 import { ImageService } from 'src/app/shared/services/image.service';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 import { element } from '@angular/core/src/render3';
+import {Img} from 'src/app/shared/services/img.model';
 
 @Component({
   selector: 'app-floorplancard',
@@ -14,6 +15,9 @@ import { element } from '@angular/core/src/render3';
 export class FloorplancardComponent implements OnInit {
 
   itemList: ImageModel[];
+  imgList: Img[];
+  imgfire: AngularFireList<any>;
+
 
   constructor(private db: AngularFireDatabase,
               private image: ImageService,
@@ -31,14 +35,32 @@ export class FloorplancardComponent implements OnInit {
         this.itemList.push(json as ImageModel);
       });
     });
+
+    this.imgfire = this.db.list('floorplanJson');
+    let data2 = this.imgfire;
+    data2.snapshotChanges().subscribe(item => {
+      this.imgList = [];
+
+      item.forEach(element => {
+        let json = element.payload.toJSON();
+        json["$key"] = element.key;
+        this.imgList.push(json as Img);
+      });
+    });
     
   }
 
-  deleteFp(key: string, i: number) {
+  deleteFp(key: string) {
     console.log(this.itemList);
-    console.log(i);
+    
+    const url = this.itemList.filter(x => x.$key == key)[0].url;
+
+    if (this.imgList.filter(x=>x.url == url).length > 0) {
+      let keyjson = this.imgList.filter(x => x.url == url)[0].$key;
+      this.imgfire.remove(keyjson);
+    }
+    
     this.image.deleteByKey(key)
-    this.ngOnInit();
   }
 
   floorplanClicked(el) {
