10813540cba551a265e067e7702d8aa65c4afe95
Cory Merrick <cmerrick02@gmail.com>
2019-04-23 21:18:42 -0400

    added role changes to plotting

--- src/app/plotting/sidenavmenu/toolbar/toolbar.component.ts
+++ src/app/plotting/sidenavmenu/toolbar/toolbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SidenavmenuComponent} from '../sidenavmenu.component';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { SnackbarService } from 'src/app/shared/snackbar.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,30 +10,54 @@ import {SidenavmenuComponent} from '../sidenavmenu.component';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor(public side: SidenavmenuComponent) { }
+  constructor(public side: SidenavmenuComponent,
+              private auth: AuthenticationService,
+              private snack: SnackbarService) { }
 
   ngOnInit() {
-    SidenavmenuComponent.EDIT_MODE = true;
-    SidenavmenuComponent.PAN_MODE = false;
-    SidenavmenuComponent.allowObjectSelect(0);
+    if (this.auth.isAdmin() == false) {
+      SidenavmenuComponent.EDIT_MODE = false;
+      SidenavmenuComponent.PAN_MODE = true;
+      SidenavmenuComponent.allowObjectSelect(0);
+      console.log('Current mode: EDIT');
+    }
+    else {
+      SidenavmenuComponent.EDIT_MODE = true;
+      SidenavmenuComponent.PAN_MODE = false;
+      SidenavmenuComponent.allowObjectSelect(0);
+      console.log('Current mode: EDIT');
+    }
   }
 
 // Function to update current canvas interaction mode (Edit or Pan mode)
   modeChanged(event) {
-    switch (event.value) {
-      case 'Edit':
-        SidenavmenuComponent.EDIT_MODE = true;
-        SidenavmenuComponent.PAN_MODE = false;
-        SidenavmenuComponent.allowObjectSelect(0);
-        break;
-      case 'Pan':
-        SidenavmenuComponent.EDIT_MODE = false;
-        SidenavmenuComponent.PAN_MODE = true;
-        SidenavmenuComponent.allowObjectSelect(1);
-        break;
-      default:
-        break;
+    console.log(event.value);
+
+    if (this.auth.isAdmin() == true) {
+
+      switch (event.value) {
+        case 'Edit':
+          SidenavmenuComponent.EDIT_MODE = true;
+          SidenavmenuComponent.PAN_MODE = false;
+          SidenavmenuComponent.allowObjectSelect(0);
+          console.log('Current mode: EDIT');
+          break;
+        case 'Pan':
+          SidenavmenuComponent.EDIT_MODE = false;
+          SidenavmenuComponent.PAN_MODE = true;
+          SidenavmenuComponent.allowObjectSelect(1);
+          console.log('Current mode: PAN');
+          break;
+        default:
+          console.log('Error: Invalid Mode Value (' + event.value + ') - toolbar.component.ts : Line 26');
+          break;
+       }
+
     }
+    else {
+      this.snack.openSnackBar('You do not have permissions for this', 2500);
+    }
+
   }
 
 // Function to update zoom level
